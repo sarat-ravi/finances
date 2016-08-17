6e48efbdf0251340f29efa9c629172cf84150c4d
Sarat Tallamraju <sarattallamraju@gmail.com>
2016-08-16 22:11:49 -0700

    Added costbasis fetcher

--- pynance/pipe.py
+++ pynance/pipe.py
@@ -8,14 +8,16 @@ class PipeException(Exception):
 class Pipe(object):
     # TODO(Sarat): Finish spec-ing this class
 
-    def __init__(self, name, flow, source_account=None, dest_account=None):
+    def __init__(self, name, flow, costbasis_fetcher=None, source_account=None, dest_account=None):
         super(Pipe, self).__init__()
         self._name = name
         assert source_account == None or isinstance(source_account, Account)
         assert dest_account == None or isinstance(dest_account, Account)
+        assert costbasis_fetcher == None or hasattr(costbasis_fetcher, '__call__')
         self._source_account = source_account
         self._dest_account = dest_account
         self._flow = flow
+        self._costbasis_fetcher = costbasis_fetcher
 
         self._blackouts = []
         self._last_flushed_t = None
@@ -60,7 +62,7 @@ class Pipe(object):
 
     def peek(self, t):
         """
-        Returns a list of pairs, where each pair is of the format (<t>, <amount>). Each pair
+        Returns a list of tuples, where each pair is of the format (<t>, <amount>, <costbasis_amount>). Each tuple
         corresponds to a transfer point.
         """
         rng = xrange(self._last_flushed_t, t)
@@ -75,7 +77,8 @@ class Pipe(object):
 
             amount = self._flow[tt]
             if amount:
-                pairs.append((tt, amount))
+                costbasis_amount = self._costbasis_fetcher(tt) if self._costbasis_fetcher else 0
+                pairs.append((tt, amount, costbasis_amount))
 
         return pairs
 
@@ -83,13 +86,13 @@ class Pipe(object):
         """
         Will flush any of the flow from self._last_flushed_t to the specified t
         """
-        for tt, amount in self.peek(t):
-            self._transfer(amount, tt)
+        for tt, amount, costbasis_amount in self.peek(t):
+            self._transfer(amount, tt, costbasis_amount)
 
         if t > self._last_flushed_t:
             self._last_flushed_t = t
 
-    def _transfer(self, amount, t):
+    def _transfer(self, amount, t, costbasis_amount):
         """
         Transfers the specified amount from the source account to the destination account.
         """
@@ -98,19 +101,18 @@ class Pipe(object):
         if self._source_account:
             self._source_account.remove(amount.security, amount.amount, t, Lot.WITHDRAW_MODE_FIFO)
 
-        #TODO(Sarat): Add costbasis information to flow
         if self._dest_account:
-            self._dest_account.add(Lot.Spot(amount.security, 0, t), amount.amount, t)
+            self._dest_account.add(Lot.Spot(amount.security, costbasis_amount, t), amount.amount, t)
 
 
 class InputPipe(Pipe):
 
-    def __init__(self, name, flow, account):
-        super(InputPipe, self).__init__(name, flow, None, account)
+    def __init__(self, name, flow, account, costbasis_fetcher=None):
+        super(InputPipe, self).__init__(name, flow, costbasis_fetcher, None, account)
 
 
 class OutputPipe(Pipe):
 
-    def __init__(self, name, flow, account):
-        super(OutputPipe, self).__init__(name, flow, account, None)
+    def __init__(self, name, flow, account, costbasis_fetcher=None):
+        super(OutputPipe, self).__init__(name, flow, costbasis_fetcher, account, None)
 
