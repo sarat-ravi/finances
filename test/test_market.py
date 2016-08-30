ff0a560a7a4c4e299bf8ce40d7e6b6b6cfbead75
Sarat Tallamraju <sarattallamraju@gmail.com>
2016-08-30 13:57:38 -0700

    Linear model tested

--- test/test_market.py
+++ test/test_market.py
@@ -1,5 +1,5 @@
 from pynance.models import Amount, Security, USD, MYR
-from pynance.market import Market, StaticMarketModel
+from pynance.market import Market, StaticMarketModel, LinearMarketModel
 from nose.tools import assert_equals, assert_not_equals
 
 class TestMarket:
@@ -10,7 +10,7 @@ class TestMarket:
     def teardown(self):
         pass
 
-    def test_static_market(self):
+    def test_static_market_model(self):
         t = 0
         market = Market("NYSE")
         model_a = StaticMarketModel("10", 10)
@@ -21,4 +21,14 @@ class TestMarket:
         market.add_market_model(model_b, 1)
 
         assert_equals(market.quote(Amount(2, USD), MYR, t+1), Amount(80, MYR))
+
+    def test_linear_market_model(self):
+        t = 0
+        market = Market("NYSE")
+        model = LinearMarketModel("y=2t+3", slope=2, initial_value=3)
+        market.add_market_model(model, 10)
+
+        for i in range(20):
+            assert_equals(market.quote(Amount(7, USD), MYR, t+i), Amount(2*i + 3, MYR))
+
 
